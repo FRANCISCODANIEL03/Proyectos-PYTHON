1e6618032fb94c65d7ab0b338a43862ba3cf3cf8
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-06-17 18:11:15 -0600

    add funcionality

--- burbuja.py
+++ burbuja.py
@@ -2,3 +2,14 @@
 ORDENAMIENTO CON ALGORITMO BURBUJA
 """
 
+def burbuja(lista):
+    for i in range(len(lista)):
+        for j in range(0,len(lista)-1-i):
+            if lista[j] > lista[j+1]:
+                aux = lista[j]
+                lista[j] = lista[j+1]
+                lista[j+1] = aux
+mi_lista = [1,4,2,6,8,6,5,3]
+burbuja(mi_lista)
+print(mi_lista)
+               
