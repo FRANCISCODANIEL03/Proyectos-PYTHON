c4bdd0541f9470d755966c4546a2f10befaf6e51
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-06-24 19:06:07 -0600

    add funcionality

--- vocal.py
+++ vocal.py
@@ -2,3 +2,12 @@
 FUNCION QUE COMPRUEBA SI UNA LETRA ES VOCAL O NO 
 """
 
+vocals = ["a","e","i","o","u"]
+def is_vocal(x):
+    if x.lower() in vocals:
+        return True
+    else:
+        return False
+
+print(is_vocal("A"))
+print(is_vocal("B"))
