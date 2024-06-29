34755f1b6951e245b6ee48a2ec9a8226980fd010
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-06-28 19:31:58 -0600

    add funcionality

--- numPares.py
+++ numPares.py
@@ -1,3 +1,11 @@
 """
 FUNCION QUE IMPRIME LOS NUMEROS PARES ENTRE DOS NUMEROS
 """
+
+def imprimir_pares(a,b):
+    for i in range(a,b+1):
+        if i % 2 == 0:
+            print(i)
+
+imprimir_pares(2,18)
+    
