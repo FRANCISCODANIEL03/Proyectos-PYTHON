d95ec6bb91485f02afc21f8f5ad1d728d746afb9
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-06-29 19:38:42 -0600

    add funcionality

--- listaRand.py
+++ listaRand.py
@@ -2,3 +2,12 @@
 FUNCION QUE CREA UNA LISTA DE N NUMEROS ENTRE UN RANGO DEFINIDO
 """
 
+import random
+
+def crear_lista(n,a,b):
+    lista = []
+    for i in range(n):
+        lista.append(random.randint(a,b))
+    return lista
+
+print(crear_lista(5,1,20))
