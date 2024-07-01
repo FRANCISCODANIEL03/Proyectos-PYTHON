a5ca74eb8771e82abe4367ac695a47d43c89451a
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-06-30 19:44:43 -0600

    add funcionality

--- multiSin.py
+++ multiSin.py
@@ -2,3 +2,11 @@
 FUNCION QUE MULTIPLIQUE 2 NUMEROS SIN UTILIZAR *
 """
 
+def multiplica(a,b):
+    total = 0
+    for i in range(a):
+        total += b
+    return total
+
+print(multiplica(3,4))
+print(multiplica(2,65))
