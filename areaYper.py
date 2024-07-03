2fa16867279d256a15da80d688863e8feeceb418
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-07-02 19:58:01 -0600

    add funcionality

--- areaYper.py
+++ areaYper.py
@@ -2,3 +2,12 @@
 FUNCION QUE DEVUELVE EL AREA Y PERIMETRO DE UN CIRCULO
 """
 
+import math
+
+def circulo(radio):
+    area = math.pi * radio ** 2
+    perim = math.pi * radio * 2
+    return area,perim
+a,p = circulo(5)
+print(f"AREA: {a}")
+print(f"PERIMETRO: {p}")
