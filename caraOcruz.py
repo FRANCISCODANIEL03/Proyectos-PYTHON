b97b0039cb1e252cbf80a037b53adda0209a2bbd
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-07-07 21:38:08 -0600

    add funcionality

--- caraOcruz.py
+++ caraOcruz.py
@@ -2,4 +2,20 @@
 SIMULAR EL JUEGO DE CARA O CRUZ
 """
 
+import random
 
+opciones = ["cara", "cruz"]
+usuario = input("cara o cruz: ").lower()
+
+if usuario not in opciones:
+    print("ERROR")
+    quit()
+
+resultado = random.choice(opciones)
+
+print("El resultado es: ", resultado)
+
+if resultado == usuario:
+    print("Has ganado!")
+else:
+    print("Has perdido")
