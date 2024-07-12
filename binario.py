de1224ac9c3667f521ab38412b47df0e5872c420
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-07-11 21:21:16 -0600

    add funcionality

--- binario.py
+++ binario.py
@@ -2,3 +2,15 @@
 PROGRAMA QUE CONVIERTE UN NUMERO DE DECIMAL A BINARIO
 """
 
+def decimal_binario(decimal):
+    if decimal == 0:
+        return "0"
+    
+    binario = ""
+    while decimal > 0:
+        res = decimal % 2
+        binario = str(res) + binario
+        decimal = decimal // 2
+    return binario
+
+print(decimal_binario(129))
