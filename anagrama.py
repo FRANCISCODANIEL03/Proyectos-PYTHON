7c52bb0761375aa1fee0142a59db179de1205b22
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-07-18 22:15:31 -0600

    add funcionality

--- anagrama.py
+++ anagrama.py
@@ -2,3 +2,14 @@
 FUNCION QUE ANALIZA DOS CADENAS DE TEXTO Y DUEVUELVE SI SON ANAGRMAS O NO
 """
 
+def es_anagrama(c1,c2):
+    if len(c1) != len(c2):
+        return False
+    
+    if sorted(c1) == sorted(c2):
+        return True
+    else:
+        return False
+    
+print(es_anagrama("boca","cabo"))
+print(es_anagrama("saul","luis"))
