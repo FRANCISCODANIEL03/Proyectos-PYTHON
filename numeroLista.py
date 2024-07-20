0099e43f3782ca3af5ef49e165b38c66228c805a
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-07-19 22:19:17 -0600

    add funcionality

--- numeroLista.py
+++ numeroLista.py
@@ -1,3 +1,11 @@
 """
 PROGRAMA QUE AVERIGUA SI UN NUMERO ESTA DENTRO DE UNA LISTA
 """
+
+lista = [1,2,3,4,8,7]
+n = 3
+
+for numero in lista:
+    if n == numero:
+        print("Esta dentro")
+    
