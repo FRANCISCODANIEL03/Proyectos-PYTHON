85e946939dc4464e6c4e60231bc940477e09663c
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-07-31 22:27:55 -0600

    organize files

---
+++ NUMBERS/reloj.py
@@ -0,0 +1,12 @@
+"""
+RELOJ QUE SE ACTUALIZA CADA SEGUNDO
+"""
+
+import time
+import os
+
+while True:
+    hora = time.strftime("%H:%M:%S")
+    os.system("cls")
+    print(hora)
+    time.sleep(1)
