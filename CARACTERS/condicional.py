54eeecf44561b7a8760faad8a2094272f410c70d
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-08-01 20:22:42 -0600

    add funcionality

--- CARACTERS/condicional.py
+++ CARACTERS/condicional.py
@@ -2,3 +2,9 @@
 PROGRAMA QUE PIDE LA EDAD Y SOLO DEJA ENTRAR SI TIENES AL MENOS DE 18 AÑOS
 """
 
+edad = int(input("Introduce tu edad: "))
+
+if edad >= 18:
+    print("Estas dentro")
+else:
+    print("no tienes la mayoria de edad")
