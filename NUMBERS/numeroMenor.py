41a119cc47417de10178cfc14f0574b09fff3302
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-08-02 20:30:41 -0600

    add funcionality

--- NUMBERS/numeroMenor.py
+++ NUMBERS/numeroMenor.py
@@ -2,3 +2,11 @@
 FUNCION QUE DEVUELVEEL NUMERO MENOR DE UNA LISTA RACIBIDA
 """
 
+def devolver_menor(lista):
+    minimo = lista[0]
+    for numero in lista:
+        if numero < minimo:
+            minimo = numero
+    return minimo
+
+print(devolver_menor([3,5,1,100,99,1000]))
