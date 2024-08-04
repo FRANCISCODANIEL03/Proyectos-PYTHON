fb14f17d4f03a9c597066dc4a9e19b41f0bbb368
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-08-03 20:40:11 -0600

    add funcionality

--- CARACTERS/usuario.py
+++ CARACTERS/usuario.py
@@ -1,3 +1,15 @@
 """
 PROGRAMA PROTEGIDO POR USUARIO Y CONTRASEÑA
 """
+
+user = "yo"
+password = "123"
+
+user_input = input("Introduce tu usuario: ")
+password_input = input("Introduce tu contraseña: ")
+
+if user != user_input or password != password_input:
+    print("Datos incorrectos!")
+    quit()
+
+print("Estas dentro")
