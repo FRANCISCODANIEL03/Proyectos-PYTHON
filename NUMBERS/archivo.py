bc7cb925afeac55424841c0d63f96f3b0bc245e1
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-08-04 21:36:48 -0600

    add funcionality

--- NUMBERS/archivo.py
+++ NUMBERS/archivo.py
@@ -2,3 +2,8 @@
 PROGRAMA QUE ESCRIBE 100 NUMEROS AL AZAR EN UN ARCHIVO DE TEXTO
 """
 
+import random
+
+with open("numero.txt","a") as archivo:
+    for i in range(100):
+        archivo.write(f"{random.randint(1,1000)}\n")
