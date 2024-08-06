1704fd00143ecce2d84fd7cf234f0bc875fd4e87
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-08-05 21:55:56 -0600

    add funcionality

--- CARACTERS/cuenta.py
+++ CARACTERS/cuenta.py
@@ -3,3 +3,21 @@ PROGRAMA QUE CONTIENE UNA CLASE CUENTA CON TITULAR, ID Y SALDO; SE PUEDE
 SACAR Y METER DINERO
 """
 
+class Cuenta:
+    def __init__(self,tiutlar,id,saldo):
+        self.titular = tiutlar
+        self.id = id
+        self.saldo = saldo
+
+    def ingreso(self,cantidad):
+        self.saldo += cantidad
+
+    def retirada(self,cantidad):
+        if cantidad <= self.saldo:
+            self.saldo -= cantidad
+
+cuenta1 = Cuenta("Luis","ES50",100)
+
+cuenta1.ingreso(350)
+cuenta1.retirada(200)
+print(cuenta1.saldo)
