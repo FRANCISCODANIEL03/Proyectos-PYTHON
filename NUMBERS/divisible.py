6ad3ed5ba415115bb709d29ed7ca9c0f4cd6e408
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-08-06 22:03:52 -0600

    add funcionality

--- NUMBERS/divisible.py
+++ NUMBERS/divisible.py
@@ -2,3 +2,12 @@
 FUNCION QUE RECIBA UN NUMERO Y DIGA SI ES DIVISIBLE POR 2, 3, O 5
 """
 
+def es_divisible(n):
+    if n % 2 == 0 or n % 3 == 0 or n % 5 == 0:
+        return True 
+    return False
+
+print(es_divisible(10))
+print(es_divisible(15))
+print(es_divisible(18))
+print(es_divisible(37))
