f8d7685211c30f2cf218f6c45165001ca7e98ea1
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-08-08 22:15:47 -0600

    add funcionality

--- NUMBERS/listaNum.py
+++ NUMBERS/listaNum.py
@@ -1,3 +1,13 @@
 """
 CODIGO QUE DADA UNA LISTA, SUSTITUYE LAS POSICIONES PARES POR "*" Y MULTIPLICA LAS IMPARES POR 100
 """
+
+lista = [1,2,3,4,89,123,42]
+
+for i in range(len(lista)):
+    if i % 2 == 0:
+        lista[i] = "*"
+    else:
+        lista[i] *= 100
+
+print(lista)
