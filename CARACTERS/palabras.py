54fcc5513260958167bd9bb40379e599309871cd
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-08-09 19:22:14 -0600

    add funcionality

--- CARACTERS/palabras.py
+++ CARACTERS/palabras.py
@@ -3,3 +3,9 @@ PROGRAMA QUE RECIBA UNA SERIE DE PALABRAS SEPARADAS POR ESPACIOS
 Y LAS CONVIERTE EN UNA LISTA
 """
 
+input_usuario = input("Introduce palabras separdas por espacios: \n")
+
+lista_palabras = input_usuario.split(" ")
+
+print(input_usuario)
+print(lista_palabras)
