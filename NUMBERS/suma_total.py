88e39861cb02d8d1fe6203b4c7448e64296c3f1e
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-08-10 19:39:18 -0600

    add funcionality

--- NUMBERS/suma_total.py
+++ NUMBERS/suma_total.py
@@ -2,3 +2,10 @@
 FUNCION QUE ACEPTE UNA LISTA DE NUMEROS Y DEVUELVA LA SUMA DE ESTOS
 """
 
+def suma_total(lista):
+    total = 0
+    for num in lista:
+        total += num
+    return total
+
+print(suma_total([1,2,3,4,5]))
