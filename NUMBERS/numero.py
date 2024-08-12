9a848502a40816d472c53c275a31308817d4d18c
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-08-11 19:46:12 -0600

    add funcionality

--- NUMBERS/numero.py
+++ NUMBERS/numero.py
@@ -2,3 +2,11 @@
 FUNCION QUE DEVUELVE EL NUMERO CONTENIDO DENTRO DE UNA FRASE
 """
 
+def devolver_numero(frase):
+    numeros = []
+    for letra in frase:
+        if letra.isdigit():
+            numeros.append(int(letra))
+    return numeros
+
+print(devolver_numero("hola que tal 3 21 yo no 2 3"))
