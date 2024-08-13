02b2ea5a3d833a6ae471b3bc10cb2b66ac47c071
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-08-12 19:50:16 -0600

    add funcionality

--- CARACTERS/apariciones.py
+++ CARACTERS/apariciones.py
@@ -2,3 +2,11 @@
 FUNCION QUE DEVUELVE CUANTAS VECES APARECE UNA LETRA EN UNA CADENA DE CARACTERES
 """
 
+def contar_letra(texto,letra):
+    contador = 0
+    for char in texto:
+        if letra == char:
+            contador += 1
+    return contador
+
+print(contar_letra("hola a todos","o"))
