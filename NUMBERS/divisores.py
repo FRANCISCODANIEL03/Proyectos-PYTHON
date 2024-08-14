7cdb8af6d45d932cc3b3df14a12f71baf5b035ef
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-08-13 20:03:29 -0600

    add funcionality

--- NUMBERS/divisores.py
+++ NUMBERS/divisores.py
@@ -2,3 +2,16 @@
 JUEGO DE DIVISORES
 """
 
+import random 
+
+divisor = random.randint(1,10)
+dividendo = random.randint(1,1000)
+
+usuario = input(f"{dividendo} / {divisor} da resto 0 ?: ")
+
+respuesta = dividendo % divisor == 0 
+
+if usuario == "si" and respuesta == True or usuario == "no" and respuesta == False:
+    print("Has ganado")
+else: 
+    print("Has perdido")
