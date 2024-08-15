b029846f4194347bf191d31675e967330700a04f
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-08-14 20:08:56 -0600

    add funcionality

--- CARACTERS/primera_letra.py
+++ CARACTERS/primera_letra.py
@@ -3,3 +3,8 @@ FUNCION QUE DADA UNA LISTA DE PALABRAS IMPRIME LA
 PRIMERA LETRA DE CADA UNA
 """
 
+def primera_letra(lista):
+    for palabra in lista:
+        print(palabra[0], palabra)
+
+primera_letra(["hola","tomate","yo"])
