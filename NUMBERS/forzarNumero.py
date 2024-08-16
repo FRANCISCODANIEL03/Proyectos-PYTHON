3173fdbf04d00e22bf41cec4887987461635eea1
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-08-15 21:58:57 -0600

    add funcionality

--- NUMBERS/forzarNumero.py
+++ NUMBERS/forzarNumero.py
@@ -2,3 +2,9 @@
 FORZAR QUE EL USUARIO INTRODUZCA UN NUMERO
 """
 
+numero = input("Introduce un numero")
+
+while not numero.isdigit():
+    numero = input("Vuelve a introducir un numero")
+
+print("Has acabado")
