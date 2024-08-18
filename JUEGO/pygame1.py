0e15cae926680053f0ad82614507b7298497e1c7
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-08-17 22:50:01 -0600

    import the class Cubo

--- JUEGO/pygame1.py
+++ JUEGO/pygame1.py
@@ -1,4 +1,5 @@
 import pygame
+from personaje import Cubo
 
 ANCHO = 800
 ALTO = 600
@@ -6,12 +7,16 @@ VENTANA = pygame.display.set_mode([ANCHO,ALTO])
 
 jugando = True
 
+cubo = Cubo(100,100)
+
 while jugando:
     eventos = pygame.event.get()
     
     for evento in eventos:
         if evento.type == pygame.QUIT:
             jugando = False
+
+    cubo.dibujar(VENTANA)
     
     pygame.display.update()
 
