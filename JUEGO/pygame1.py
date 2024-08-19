cde169b13469aa8dc211e06ca277c617f199e2ba
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-08-18 22:59:25 -0600

    add moviment at cube

--- JUEGO/pygame1.py
+++ JUEGO/pygame1.py
@@ -9,12 +9,27 @@ jugando = True
 
 cubo = Cubo(100,100)
 
+def gestionar_teclas(teclas):
+    if teclas[pygame.K_w]:
+        cubo.y -= cubo.velocidad
+    if teclas[pygame.K_s]:
+        cubo.y += cubo.velocidad
+    if teclas[pygame.K_a]:
+        cubo.x -= cubo.velocidad
+    if teclas[pygame.K_d]:
+        cubo.x += cubo.velocidad
+
 while jugando:
     eventos = pygame.event.get()
+
+    teclas = pygame.key.get_pressed()
+
+    gestionar_teclas(teclas)
     
     for evento in eventos:
         if evento.type == pygame.QUIT:
             jugando = False
+
 
     cubo.dibujar(VENTANA)
     
