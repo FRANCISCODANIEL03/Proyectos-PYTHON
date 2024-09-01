63e10bd9ef370a47d3c7923a0cd6e2aaaf354499
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-08-31 22:47:58 -0600

    add images

--- JUEGO/item.py
+++ JUEGO/item.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 class Item:
     def __init__(self, x, y):
@@ -7,16 +8,17 @@ class Item:
         self.ancho = 30
         self.alto = 30
         self.velocidad = 5
-        self.color = "purple"
+        self.tipo = random.randint(1,2)
+        #self.color = "purple" if self.tipo == 1 else "green"
+        self.img = "bullet.png" if self.tipo == 1 else "enemy.png"
         self.rect = pygame.Rect(self.x, self.y, self.ancho, self.alto)
-        self.vida = 4
-        #self.imagen = pygame.image.load("JUEGO/IMGS/enemy.png").convert()
-        #self.imagen = pygame.transform.scale(self.imagen, (self.ancho, self.alto))
+        self.imagen = pygame.image.load(f"JUEGO/IMGS/{self.img}").convert()
+        self.imagen = pygame.transform.scale(self.imagen, (self.ancho, self.alto))
 
     def dibujar(self, ventana):
         self.rect = pygame.Rect(self.x, self.y, self.ancho, self.alto)
-        pygame.draw.rect(ventana, self.color, self.rect)
-        #ventana.blit(self.imagen, (self.x, self.y))
+        #pygame.draw.rect(ventana, self.color, self.rect)
+        ventana.blit(self.imagen, (self.x, self.y))
 
     def movimiento(self):
         self.y += self.velocidad
