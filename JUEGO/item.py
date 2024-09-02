a08b8e83fd4fad9be5a4eb50b5a9a4921cfd73af
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-09-01 23:11:03 -0600

    add new funcionality

--- JUEGO/item.py
+++ JUEGO/item.py
@@ -5,15 +5,16 @@ class Item:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.ancho = 30
-        self.alto = 30
-        self.velocidad = 5
         self.tipo = random.randint(1,2)
+        self.ancho = 30 if self.tipo == 2 else 40
+        self.alto = 30 if self.tipo == 2 else 20
+        self.velocidad = 5
         #self.color = "purple" if self.tipo == 1 else "green"
-        self.img = "bullet.png" if self.tipo == 1 else "enemy.png"
+        self.img = "max_disparo.png" if self.tipo == 1 else "max_vel.png"
         self.rect = pygame.Rect(self.x, self.y, self.ancho, self.alto)
         self.imagen = pygame.image.load(f"JUEGO/IMGS/{self.img}").convert()
         self.imagen = pygame.transform.scale(self.imagen, (self.ancho, self.alto))
+        self.imagen = pygame.transform.rotate(self.imagen, 90) if self.tipo == 1 else pygame.transform.rotate(self.imagen, 360)
 
     def dibujar(self, ventana):
         self.rect = pygame.Rect(self.x, self.y, self.ancho, self.alto)
