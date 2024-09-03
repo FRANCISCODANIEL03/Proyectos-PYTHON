8de190e193c38efbca937b1c457c103eb86846e4
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-09-02 19:09:28 -0600

    add new functions

--- JUEGO/item.py
+++ JUEGO/item.py
@@ -5,12 +5,17 @@ class Item:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.tipo = random.randint(1,2)
-        self.ancho = 30 if self.tipo == 2 else 40
-        self.alto = 30 if self.tipo == 2 else 20
+        self.tipo = random.randint(1,3)
+        self.ancho = 40 if self.tipo == 1  else 30
+        self.alto = 20 if self.tipo == 1  else 30
         self.velocidad = 5
         #self.color = "purple" if self.tipo == 1 else "green"
-        self.img = "max_disparo.png" if self.tipo == 1 else "max_vel.png"
+        if self.tipo == 1:
+            self.img = "max_disparo.png"
+        elif self.tipo == 2:
+            self.img = "max_vel.png"
+        else:
+            self.img = "vida.png"
         self.rect = pygame.Rect(self.x, self.y, self.ancho, self.alto)
         self.imagen = pygame.image.load(f"JUEGO/IMGS/{self.img}").convert()
         self.imagen = pygame.transform.scale(self.imagen, (self.ancho, self.alto))
