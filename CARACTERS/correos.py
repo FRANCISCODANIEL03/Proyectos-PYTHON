46e4a775835561027b5588d79d8d57cc6d60b48a
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-09-19 21:32:08 -0600

    add funcionality

--- CARACTERS/correos.py
+++ CARACTERS/correos.py
@@ -2,3 +2,8 @@
 Genera 100 correos numerados con un nombre
 """
 
+nombre = input("Introduce tu nombre: ")
+veces = int(input("Introduce el numero de correos: "))
+
+for i in range(veces):
+    print(f"{nombre}{i+1}@gmail.com")
