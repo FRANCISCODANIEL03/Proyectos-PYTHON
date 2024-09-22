a77e0ccbb97dda101ba1a38263d6d46d2ae61676
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-09-21 21:54:45 -0600

    add funcionality

--- DATA_BASE/mysql1.py
+++ DATA_BASE/mysql1.py
@@ -1,3 +1,19 @@
 """
 Crear una conexion a una base de datos
 """
+
+import mysql.connector
+
+db = mysql.connector.connect(
+    user="root",
+    password="root",
+    host="localhost",
+    database="baseapi"
+)
+
+cursor = db.cursor()
+cursor.execute("SELECT * FROM User")
+result = cursor.fetchall()
+
+for user in result:
+    print(user)
