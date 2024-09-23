d55e5e723bef0fcf98fa1c942a327c3b4c68e642
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-09-22 22:01:07 -0600

    add new option

--- DATA_BASE/mysql1.py
+++ DATA_BASE/mysql1.py
@@ -12,8 +12,19 @@ db = mysql.connector.connect(
 )
 
 cursor = db.cursor()
+""" SELECCIONAR TODA LA INFORMACION DE UNA TABLA
 cursor.execute("SELECT * FROM User")
 result = cursor.fetchall()
 
 for user in result:
     print(user)
+"""
+
+sql = "SELECT username FROM user WHERE id = %s"
+
+par = (152,)
+
+cursor.execute(sql, par)
+
+result = cursor.fetchone()[0]
+print(result)
