fcdb510012802c51e3d02d5b1ddd8dc2bd07a0a4
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-09-24 22:15:13 -0600

    add funcionality

--- CARACTERS/burbuja.py
+++ CARACTERS/burbuja.py
@@ -2,3 +2,14 @@
 Ordenamiento por metodo burbuja
 """
 
+enteros = [1,5,7,12,23,2,4,563,2,3,45,23,12,34,9,8,79]
+print(enteros)
+
+for i in range(len(enteros)):
+    for j in range(len(enteros)-i-1):
+        if enteros[j] > enteros[j+1]:
+            aux = enteros[j]
+            enteros[j] = enteros[j+1]
+            enteros[j+1] = aux
+
+print(enteros)
