d9d1912c21cec5de198a9ea12536ff60499b5acc
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-09-27 20:36:44 -0600

    add funcionality

--- NUMBERS/calculadora.py
+++ NUMBERS/calculadora.py
@@ -2,3 +2,18 @@
 Calculadora
 """
 
+num1 = int(input("Numero 1: "))
+num2 = int(input("Numero 2: "))
+operacion = input("Introduce una operacion(+ - * /)")
+
+match operacion:
+    case "+":
+        res = num1 + num2
+    case "-":
+        res = num1 - num2
+    case "*":
+        res = num1 * num2
+    case "/":
+        res = num1 / num2
+
+print(f"El resultado de {num1} {operacion} {num2} = {res}")
