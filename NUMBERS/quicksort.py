c5d70fa4c60e9b29d86c7cfcdd7df692aadad941
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-09-28 21:21:20 -0600

    add funcionality

--- NUMBERS/quicksort.py
+++ NUMBERS/quicksort.py
@@ -2,3 +2,17 @@
 Ordenamiento quicksort
 """
 
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    pivote = arr[len(arr)//2]
+    izq = [x for x in arr if x < pivote]
+    centro = [x for x in arr if x == pivote]
+    der = [x for x in arr if x > pivote]
+
+    return quick_sort(izq) + quick_sort(centro) + quick_sort(der)
+
+array = [1,4,6,45,67,3,23,12]
+print(array)
+print(quick_sort(array))
