1d3a74245bad0a9ca46293eb486bfa5cec9245f9
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-09-29 21:31:04 -0600

    add funcionality

--- DATA_BASE/sqlhash.py
+++ DATA_BASE/sqlhash.py
@@ -2,4 +2,12 @@
 Hashear una contraseña para hacerla segura
 """
 
+import bcrypt
 
+password = "pako123"
+password_encoded = password.encode()
+
+salt = bcrypt.gensalt(15)
+
+hashed_password = bcrypt.hashpw(password_encoded, salt)
+print(hashed_password)
