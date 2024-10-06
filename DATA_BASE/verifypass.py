b4c84250c78a7a7fa90bb828ad6e9abc172da810
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-10-05 21:46:40 -0600

    add funcionality

--- DATA_BASE/verifypass.py
+++ DATA_BASE/verifypass.py
@@ -2,3 +2,27 @@
 Extraer una contraseña hasheada para veficarla
 """
 
+import brcrypt
+import mysql
+
+db = mysql.connector.connect(
+    user="root",
+    password="root",
+    host="localhost",
+    database="baseapi"
+)
+
+cursor = db.cursor()
+
+user = input("User: ")
+password = input("Password: ")
+
+sql = "SELECT * FROM user WHERE username = %s"
+
+cursor.execute(sql, (user, ))
+
+hashed_password = cursor.fetchone()[2]
+
+isLogin = bcrypt.checkpw(password.encode(), hashed_password.encode())
+
+print(isLogin)
