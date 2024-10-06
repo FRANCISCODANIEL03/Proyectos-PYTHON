04dc2ae8f91b60750b113a344f964be7711e0cef
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-10-05 21:37:09 -0600

    add connection with db and insert hashed password

--- DATA_BASE/sqlhash.py
+++ DATA_BASE/sqlhash.py
@@ -3,11 +3,29 @@ Hashear una contraseña para hacerla segura
 """
 
 import bcrypt
+import mysql
 
-password = "pako123"
-password_encoded = password.encode()
+def hash_password(passw):
+    password_encoded = passw.encode()
 
-salt = bcrypt.gensalt(15)
+    salt = bcrypt.gensalt(15)
 
-hashed_password = bcrypt.hashpw(password_encoded, salt)
-print(hashed_password)
+    hashed_password = bcrypt.hashpw(password_encoded, salt)
+    print(hashed_password)
+
+
+db = mysql.connector.connect(
+    user="root",
+    password="root",
+    host="localhost",
+    database="baseapi"
+)
+
+cursor = db.cursor()
+
+sql = "INSERT user (username, password) VALUES (%s, %s)"
+
+values = ("pako03", hash_password("parro324"))
+
+cursor.execute(sql, values)
+db.commit()
