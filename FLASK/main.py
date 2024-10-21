9656e4a1e5d0a07436a8b352389762ac1ab04c62
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-10-20 21:20:01 -0600

    add API rest

--- FLASK/main.py
+++ FLASK/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -9,4 +9,16 @@ def home():
 @app.route("/hola/<user>")
 def hola(user):
     return render_template("hola.html", nombre=user)
+
+@app.route("/usuarios", methods=["GET"])
+def usuarios():
+    usuarios = [
+        {"id": 1, 
+         "username":"yo",
+         "email": "user1@gmail.com"},
+        {"id": 2, 
+         "username":"yo2",
+         "email": "user2@gmail.com"}
+    ]
+    return jsonify(usuarios)
 app.run()
