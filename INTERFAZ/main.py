37933414c0e539df8a5a5253e474f29c1896f238
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2024-11-14 21:45:08 -0600

    add button

--- INTERFAZ/main.py
+++ INTERFAZ/main.py
@@ -6,4 +6,10 @@ ventana.geometry("300x300")
 
 etiqueta = tk.Label(ventana, text="Esto es una etiqueta")
 etiqueta.pack(pady=100)
+
+def click():
+    print("Me haz hecho click")
+
+boton = tk.Button(ventana, text="Click", command=click)
+boton.pack()
 ventana.mainloop()
