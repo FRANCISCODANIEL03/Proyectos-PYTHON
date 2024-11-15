ca01d7eae25ba59becad52ff624044194ae1efe3
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2024-11-14 22:03:08 -0600

    add function that sums two numbers

--- INTERFAZ/input_user.py
+++ INTERFAZ/input_user.py
@@ -4,12 +4,20 @@ ventana = tk.Tk()
 ventana.title("Titulo")
 ventana.geometry("300x300")
 
-user = tk.Entry(ventana)
-user.pack()
+fnt=("Arial", 25)
+num1 = tk.Entry(ventana)
+num1.pack()
+
+num2 = tk.Entry(ventana)
+num2.pack()
+
+result_label = tk.Label(ventana, text="Resultado: ", font=fnt)
+result_label.pack()
 
 def add():
-    val = user.get()
-    print(val)
+    nm1 = int(num1.get())
+    nm2 = int(num2.get())
+    result_label.config(text=f"Resultado: {nm1+nm2}")
     
 btn = tk.Button(ventana, text="add", command=add)
 btn.pack()
