0f16436d9aeae76341a126b7dd398f0e50b00fb9
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2024-11-23 20:47:06 -0600

    create labels and inputs

--- INTERFAZ/agenda.py
+++ INTERFAZ/agenda.py
@@ -6,6 +6,10 @@ import tkinter as tk
 win = tk.Tk()
 win.geometry("600x400")
 
+input_name = tk.Entry(win)
+input_phone = tk.Entry(win)
+label_name = tk.Label(win, text="Nombre: ")
+label_phone = tk.Label(win, text="Telefono: ")
 
 
 
