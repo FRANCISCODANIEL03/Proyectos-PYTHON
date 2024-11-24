23f1c7375f5f4242c40c7b44a84d97284c354f2e
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2024-11-23 21:04:42 -0600

    add treeview

--- INTERFAZ/agenda.py
+++ INTERFAZ/agenda.py
@@ -2,6 +2,7 @@
 AGENDA - GESTION DE CONTACTOS
 '''
 import tkinter as tk
+from tkinter import ttk
 
 win = tk.Tk()
 win.geometry('600x400')
@@ -10,6 +11,8 @@ input_name = tk.Entry(win)
 input_phone = tk.Entry(win)
 label_name = tk.Label(win, text='Name: ')
 label_phone = tk.Label(win, text='Phone: ')
+tree_agenda = ttk.Treeview(win, columns=('id', 'Name', 'Phone'), show='headings')
+
 
 contacts = []
 
@@ -28,6 +31,6 @@ input_name.pack(pady=(0, 20))
 label_phone.pack()
 input_phone.pack(pady=(0, 20))
 btn_add.pack()
-
+tree_agenda.pack()
 
 win.mainloop()
