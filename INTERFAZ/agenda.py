9f0c19ef6d1f367a36e1f9e55bae61888a72266a
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2024-11-23 20:57:36 -0600

    add new button

--- INTERFAZ/agenda.py
+++ INTERFAZ/agenda.py
@@ -1,20 +1,24 @@
-"""
+'''
 AGENDA - GESTION DE CONTACTOS
-"""
+'''
 import tkinter as tk
 
 win = tk.Tk()
-win.geometry("600x400")
+win.geometry('600x400')
 
 input_name = tk.Entry(win)
 input_phone = tk.Entry(win)
-label_name = tk.Label(win, text="Nombre: ")
-label_phone = tk.Label(win, text="Telefono: ")
+label_name = tk.Label(win, text='Name: ')
+label_phone = tk.Label(win, text='Phone: ')
+
+btn_add = tk.Button(win, text='Add contact', command=add)
+
 
 label_name.pack()
-input_name.pack(pady=20)
+input_name.pack(pady=(0, 20))
 label_phone.pack()
-input_phone.pack(pady=20)
+input_phone.pack(pady=(0, 20))
+btn_add.pack()
 
 
 win.mainloop()
