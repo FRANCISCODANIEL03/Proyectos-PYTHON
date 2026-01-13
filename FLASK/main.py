34f225358f9f62eb28acc8ec523853ab2487e0fd
Francisco Daniel Gregorio Guevara <160035631+FRANCISCODANIEL03@users.noreply.github.com>
2026-01-12 20:02:45 -0600

    Fix typo in template file name

--- FLASK/main.py
+++ FLASK/main.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("indes.html")
+    return render_template("index.html")
 
 @app.route("/productos", methods=["GET"])
 def getproducts():
