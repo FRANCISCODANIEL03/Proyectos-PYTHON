eef7f1b69a473ab9936552821b321fdd1fdfe9af
Francisco Daniel Gregorio Guevara <160035631+FRANCISCODANIEL03@users.noreply.github.com>
2026-01-14 20:59:18 -0600

    Add main check for running Flask app

--- FLASK/main.py
+++ FLASK/main.py
@@ -46,4 +46,6 @@ def editproductos(id):
             productos[idx] = nuevoProducto
             return "Producto editado correctamente"
     return "Producto no encontrado"
-app.run()
+    
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=3000, debug=False)
